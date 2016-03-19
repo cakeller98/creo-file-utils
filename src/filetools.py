183641db7b939488b65381991715e5a920c1bf65
admin <loleven@gmail.com>
2016-03-19 12:35:38 +0100

    Made it into a class

--- src/filetools.py
+++ src/filetools.py
@@ -7,27 +7,41 @@ import time
 import pathlib
 
 
-patterns = ['*.prt.*', '*.asm.*', '*.drw.*', '*.lay.*']
+class creo_file_tool:
+    def __init__(self, output):
+        self.patterns = ['*.prt.*', '*.asm.*', '*.drw.*', '*.lay.*']
+        self.temp_env = ['TMPDIR', 'TEMP', 'TMP']
 
-def rename_files(folder,sub_folders=False):
-    print("Rename files")
+        self.temp_folder = None
 
-    con = sqlite3.connect(r'c:\temp\slask.db3')
-
-
-    insertstmt = 'INSERT INTO files VALUES (?,?,?,?)'
-    selectstmt_01 = 'select name, ext, version from files group by name,ext'
-    selectstmt_02 = 'select * from files where name=? and ext=? and version<=?'
+        self.insertstmt = 'INSERT INTO files VALUES (?,?,?,?)'
+        self.selectstmt_01 = 'select name, ext, version from files group by name,ext'
+        self.selectstmt_02 = 'select * from files where name=? and ext=? and version<=?'
 
 
-    with con:
-        cur = con.cursor()
+        self.drop_stmt = "DROP TABLE IF EXISTS files"
+        self.create_stmt = 'create table files (filename varchar(100), name varchar(100),ext varchar(100),version bigint)'
 
-        cur.execute("DROP TABLE IF EXISTS files")
-        cur.execute('create table files (filename varchar(100), name varchar(100),ext varchar(100),version bigint)')
-        con.commit()
+        for temp_str in self.temp_env:
+            if self.temp_folder is None and os.environ.get(temp_str) is not None:
+                self.temp_folder = os.environ.get(temp_str)
 
-        for pattern in patterns:
+        self.print_out=output
+
+    def init_db(self):
+        self.con = sqlite3.connect(r'c:\temp\slask.db3')
+        # self.con = sqlite3.connect(':memory:')
+
+    def rename_files(self, folder, sub_folders=False):
+        print("Rename files")
+
+        cur = self.con.cursor()
+
+        cur.execute(self.drop_stmt)
+        cur.execute(self.create_stmt)
+        self.con.commit()
+
+        for pattern in self.patterns:
             searchfor = r'{0}\{1}'.format(folder, pattern)
 
             print("Sök: " + searchfor)
@@ -47,40 +61,31 @@ def rename_files(folder,sub_folders=False):
 
                 if strtmp[2].isdigit():
                     parameters = (str(file), strtmp[0] + strtmp[1], strtmp[1], strtmp[2])
-                    cur.execute(insertstmt, parameters)
+                    cur.execute(self.insertstmt, parameters)
 
-        con.commit()
+        self.con.commit()
 
-def purge_files(folder, backup=True, keepversion=1, sub_folders=False):
-    print("Purge files")
+    def purge_files(self, folder, backup=True, keepversion=1, sub_folders=False):
+        print("Purge files")
 
-    con = sqlite3.connect(r'c:\temp\slask.db3')
-    # con = sqlite3.connect(':memory:')
+        cur = self.con.cursor()
 
-    insertstmt = 'INSERT INTO files VALUES (?,?,?,?)'
-    selectstmt_01 = 'select name, ext, version from files group by name,ext'
-    selectstmt_02 = 'select * from files where name=? and ext=? and version<=?'
+        cur.execute(self.drop_stmt)
+        cur.execute(self.create_stmt)
+        self.con.commit()
 
-    with con:
-        cur = con.cursor()
-
-        cur.execute("DROP TABLE IF EXISTS files")
-        cur.execute('create table files (filename varchar(100), name varchar(100),ext varchar(100),version bigint)')
-        con.commit()
-
-
-        for pattern in patterns:
+        for pattern in self.patterns:
             searchfor = r'{0}\{1}'.format(folder, pattern)
 
-            print("Sök: "+searchfor)
+            print("Sök: " + searchfor)
 
             if sub_folders:
                 files = pathlib.Path(folder).glob('**/{0}'.format(pattern))
             else:
-                files=pathlib.Path(folder).glob(pattern)
+                files = pathlib.Path(folder).glob(pattern)
 
             for file in files:
-                p=pathlib.WindowsPath(file)
+                p = pathlib.WindowsPath(file)
                 print(file)
                 print(p.parent)
                 print(file.name)
@@ -88,12 +93,12 @@ def purge_files(folder, backup=True, keepversion=1, sub_folders=False):
                 print(strtmp)
 
                 if strtmp[2].isdigit():
-                    parameters = (str(file), strtmp[0]+strtmp[1], strtmp[1], strtmp[2])
-                    cur.execute(insertstmt, parameters)
+                    parameters = (str(file), strtmp[0] + strtmp[1], strtmp[1], strtmp[2])
+                    cur.execute(self.insertstmt, parameters)
 
-        con.commit()
+        self.con.commit()
 
-        cur.execute(selectstmt_01)
+        cur.execute(self.selectstmt_01)
         rows = cur.fetchall()
 
         for row in rows:
@@ -103,23 +108,24 @@ def purge_files(folder, backup=True, keepversion=1, sub_folders=False):
 
             print('{0} {1} {2}'.format(file, ext, version))
 
-            cur1 = con.cursor()
-            cur1.execute(selectstmt_02, (file, ext, version - keepversion))
+            cur1 = self.con.cursor()
+            cur1.execute(self.selectstmt_02, (file, ext, version - keepversion))
             rows02 = cur1.fetchall()
 
             for row02 in rows02:
                 deletefile = row02[0]
 
-                dir_str=os.path.dirname(deletefile)
+                dir_str = os.path.dirname(deletefile)
 
                 if backup:
-                    os.makedirs(r'{0}\Backup'.format(dir_str),exist_ok=True)
+                    os.makedirs(r'{0}\Backup'.format(dir_str), exist_ok=True)
 
                 try:
-                    print('Deleting: '+deletefile)
+                    print('Deleting: ' + deletefile)
+                    self.print_out.print_str('Deleting: ' + deletefile)
 
                     if backup:
-                        shutil.move(deletefile,r'{0}\Backup'.format(dir_str))
+                        shutil.move(deletefile, r'{0}\Backup'.format(dir_str))
                     else:
                         print(deletefile)
                         # os.remove(deletefile)
