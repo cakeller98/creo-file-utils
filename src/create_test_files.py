4c938f8cdb1df45cff728915a986b016cb9cc9ea
Lars-Olof <loleven@gmail.com>
2016-04-13 12:01:02 +0200

    Added more models, drawing etc. to create for testing

--- src/create_test_files.py
+++ src/create_test_files.py
@@ -2,7 +2,8 @@ import os
 import random
 import shutil
 
-filesList = ['test_d.drw', 'test_a.asm', 'test_p.prt', 'test_l.lay']
+filesList = ['test_d_a.drw', 'test_d_b.drw', 'test_a_a.asm', 'test_a_b.asm', 'test_p_a.prt', 'test_p_b.prt',
+             'test_l_a.lay', 'test_l_b.lay']
 folderList = [r'model', r'model\level_1', r'model\level_1\level_2']
 
 if os.path.exists(folderList[0]):
